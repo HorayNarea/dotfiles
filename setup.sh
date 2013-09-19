532f6ff3e2347c4bfd76811dd6f43d79e2299fad
Thomas Sänger <HorayNarea@gmail.com>
2013-09-19 02:15:30 +0200

    setup backups old configfiles if they exist

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/htoprc ~/.config/htop/htoprc
-sudo ln -s ~/dotfiles/zsh/* /etc/zsh/.
+#!/bin/sh
+
+GIT=~/.gitconfig
+HTOP=~/.config/htop/htoprc
+
+if [ -f $GIT ]; then
+	mv $GIT $GIT.bak
+fi
+ln -s ~/dotfiles/gitconfig $GIT
+
+if [ -f $HTOP ]; then
+	mv $HTOP $HTOP.bak
+fi
+ln -s ~/dotfiles/htoprc $HTOP
+
+if [ -d /etc/zsh ]; then
+	sudo mv /etc/zsh /etc/zsh.bak
+fi
+sudo ln -s ~/dotfiles/zsh /etc/zsh
