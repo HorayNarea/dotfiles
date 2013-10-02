863c7179ad6530e45a74e1ebd6a259ca4d44565f
Thomas Sänger <HorayNarea@gmail.com>
2013-10-02 06:47:18 +0200

    added aria2.conf

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ DOTFILES=~/.dotfiles
 GIT=~/.gitconfig
 HTOP=~/.config/htop/htoprc
 SCREEN=~/.screenrc
+ARIA2=~/.aria2
 
 mv $GIT $GIT.bak > /dev/null
 ln -s $DOTFILES/gitconfig $GIT
@@ -12,6 +13,9 @@ ln -s $DOTFILES/htoprc $HTOP
 
 mv $SCREEN $SCREEN.bak > /dev/null
 ln -s $DOTFILES/screenrc $SCREEN
+
+mv $ARIA2 $ARIA2.bak > /dev/null
+ln -s $DOTFILES/aria2 $ARIA2
 
 sudo mv /etc/zsh /etc/zsh.bak > /dev/null
 sudo ln -s $DOTFILES/zsh /etc/zsh
