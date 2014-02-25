cbdaa35df51f368ba5be700e61acd6fb1f5bca0e
Thomas Sänger <HorayNarea@gmail.com>
2014-02-25 07:20:45 +0100

    better screenrc & setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ DOTFILES=~/.dotfiles
 GIT=~/.gitconfig
 HTOP=~/.config/htop/htoprc
 ARIA2=~/.aria2
+ZSH=~/.zshrc
+SCREEN=~/.screenrc
 
 git submodule init
 git submodule update
@@ -19,6 +21,9 @@ ln -s $DOTFILES/aria2 $ARIA2
 sudo mv -f /etc/zsh /etc/zsh.bak > /dev/null
 sudo cp -r $DOTFILES/zsh /etc/zsh
 
-mv -f ~/.zshrc ~/.zshrc.bak > /dev/null
+mv -f $ZSH $ZSH.bak > /dev/null
 ln -s $DOTFILES/zshrc ~/.zshrc
 rm -f ~/.zcompdump*
+
+mv -f $SCREEN $SCREEN.bak > /dev/null
+ln -s $DOTFILES/screenrc $SCREEN
