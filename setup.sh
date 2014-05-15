e9a50aac063d0982abc44e21b41e8d9757101ee7
Thomas Sänger <HorayNarea@gmail.com>
2014-05-15 19:35:58 +0200

    force symlink

--- setup.sh
+++ setup.sh
@@ -12,26 +12,26 @@ git submodule init
 git submodule update
 
 mv -f $GIT $GIT.bak > /dev/null
-ln -s $DOTFILES/gitconfig $GIT
+ln -fs $DOTFILES/gitconfig $GIT
 
 mv -f $HTOP $HTOP.bak > /dev/null
-ln -s $DOTFILES/htoprc $HTOP
+ln -fs $DOTFILES/htoprc $HTOP
 
 mv $ARIA2 $ARIA2.bak > /dev/null
-ln -s $DOTFILES/aria2 $ARIA2
+ln -fs $DOTFILES/aria2 $ARIA2
 
 sudo mv -f /etc/zsh /etc/zsh.bak > /dev/null
 sudo cp -r $DOTFILES/zsh /etc/zsh
 
 mv -f $ZSH $ZSH.bak > /dev/null
-ln -s $DOTFILES/zshrc $ZSH
+ln -fs $DOTFILES/zshrc $ZSH
 rm -f ~/.zcompdump*
 
 mv -f $SCREEN $SCREEN.bak > /dev/null
-ln -s $DOTFILES/screenrc $SCREEN
+ln -fs $DOTFILES/screenrc $SCREEN
 
 mv -f $CONKY $CONKY.bak > /dev/null
-ln -s $DOTFILES/conkyrc $CONKY
+ln -fs $DOTFILES/conkyrc $CONKY
 
 mv -f $COLORDIFF $COLORDIFF.bak > /dev/null
-ln -s $DOTFILES/colordiffrc $COLORDIFF
+ln -fs $DOTFILES/colordiffrc $COLORDIFF
