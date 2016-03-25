37544d5a0d36bdf9613e160487185dcaf609ee6c
Thomas Sänger <HorayNarea@gmail.com>
2016-03-25 05:28:49 +0100

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 git clone https://github.com/tarjoilija/zgen ~/.dotfiles/zgen
 
 # global
-# gitconfig zshrc screenrc config_htop_htoprc ssh_config
 for source in files/global/*; do
 	for target in $(echo ${source}|sed "s,files/global/,,"|tr "_" "/"); do
 		mv -vf ~/.${target} ~/.${target}.bak
@@ -13,11 +12,13 @@ done
 
 # host-specific
 HOST=$(hostname)
-for source in files/host-specific/${HOST}/*; do
-	for target in $(echo ${source}|sed "s,files/host-specific/${HOST}/,,"|tr "_" "/"); do
-		mv -vf ~/.${target} ~/.${target}.bak
-		ln -vfs ~/.dotfiles/${source} ~/.${target}
+if [ -d files/host-specific/${HOST} ]; then
+	for source in files/host-specific/${HOST}/*; do
+		for target in $(echo ${source}|sed "s,files/host-specific/${HOST}/,,"|tr "_" "/"); do
+			mv -vf ~/.${target} ~/.${target}.bak
+			ln -vfs ~/.dotfiles/${source} ~/.${target}
+		done
 	done
-done
+fi
 
 mkdir ~/.zsh
