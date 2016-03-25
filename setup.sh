f2a24cb833e01ba1d3a3072d66bc54a15d772a33
Thomas Sänger <HorayNarea@gmail.com>
2016-03-25 04:43:19 +0100

    add sift.conf

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/tarjoilija/zgen ~/.dotfiles/zgen
 
-for source in gitconfig zshrc screenrc conkyrc config_htop_htoprc aria2_aria2.conf ssh_config; do
+for source in gitconfig zshrc screenrc conkyrc config_htop_htoprc aria2_aria2.conf ssh_config sift.conf; do
 	for target in $(echo ${source}|tr "_" "/"); do
 		mv -vf ~/.${target} ~/.${target}.bak
 		ln -vfs ~/.dotfiles/${source} ~/.${target}
